21051deb240a7d969b67733f208d3ca28b2bd633
sc0der <siriusproger@gmail.com>
2022-06-03 18:59:49 +0500

    updated swagger

--- auth-service/app/main.py
+++ auth-service/app/main.py
@@ -1,10 +1,26 @@
 
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
+from fastapi import applications
+from fastapi.openapi.docs import get_swagger_ui_html
 
 from app.api.api_v1.api import api_router
 from app.core.config import settings
 
+
+def swagger_monkey_patch(*args, **kwargs):
+    """
+    Wrap the function which is generating the HTML for the /docs endpoint and 
+    overwrite the default values for the swagger js and css.
+    """
+    return get_swagger_ui_html(
+        *args, **kwargs,
+        swagger_js_url="https://cdn.jsdelivr.net/npm/swagger-ui-dist@3.29/swagger-ui-bundle.js",
+        swagger_css_url="https://cdn.jsdelivr.net/npm/swagger-ui-dist@3.29/swagger-ui.css")
+
+
+# Actual monkey patch
+applications.get_swagger_ui_html = swagger_monkey_patch
 app = FastAPI(
     title="settings.PROJECT_NAM", openapi_url=f"{settings.API_V1_STR}/openapi.json"
 )
--- routes-service/app/main.py
+++ routes-service/app/main.py
@@ -1,10 +1,27 @@
 from fastapi import FastAPI
+from fastapi import applications
+from fastapi.openapi.docs import get_swagger_ui_html
+
 from app.api.api_v1.api import api_router
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 1
 
+
+def swagger_monkey_patch(*args, **kwargs):
+    """
+    Wrap the function which is generating the HTML for the /docs endpoint and 
+    overwrite the default values for the swagger js and css.
+    """
+    return get_swagger_ui_html(
+        *args, **kwargs,
+        swagger_js_url="https://cdn.jsdelivr.net/npm/swagger-ui-dist@3.29/swagger-ui-bundle.js",
+        swagger_css_url="https://cdn.jsdelivr.net/npm/swagger-ui-dist@3.29/swagger-ui.css")
+
+
+# Actual monkey patch
+applications.get_swagger_ui_html = swagger_monkey_patch
 
 app = FastAPI()
 app.include_router(api_router)
